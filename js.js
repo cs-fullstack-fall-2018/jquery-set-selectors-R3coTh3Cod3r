22e19ede18a5254a86d610c83bf48ac01207bd2a
Mareco Nabors <naborsmarico99@gmail.com>
2018-08-17 11:47:37 -0500

    jquery select

---
+++ js.js
@@ -0,0 +1,4 @@
+$("h1").css("border","2px solid yellow");
+$("#intro").css("border","2px solid yellow");
+$("#page-content").css("border","2px solid yellow")
+$("#gallery").css("border","2px solid yellow")
